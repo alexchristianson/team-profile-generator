ec4d2810f07d051b7170044a4bc35ee45489386b
Alex C <alex.christianson.05@gmail.com>
2021-10-29 14:52:23 -0500

    added Intern constructor and tests

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,4 +1,3 @@
-const { test, expect } = require('@jest/globals');
 const Engineer = require('../lib/Engineer');
 
 test('can set github username via constructor', () => {
---
+++ __tests__/Intern.test.js
@@ -0,0 +1,23 @@
+const { test, expect } = require('@jest/globals');
+const Intern = require('../lib/Intern');
+
+test('can get school via constructor', () => {
+    const testSchool = "UW-Madison";
+    const intern = new Intern("Bob", 111, "bob@email.com", testSchool);
+
+    expect(intern.school).toEqual(testSchool);
+});
+
+test('get school via getSchool()', () => {
+    const testSchool = "UW-Madison";
+    const intern = new Intern("Bob", 111, "bob@email.com", testSchool);
+
+    expect(intern.getSchool()).toEqual(testSchool);
+});
+
+test('getRole() should return "Intern"', () => {
+    const testRole = "Intern";
+    const intern = new Intern("Bob", 111, "bob@email.com", "UW-Madison");
+
+    expect(intern.getRole()).toEqual(testRole);
+})
