266906c22e8d09bc8b40e91f428f79e5fcb985ab
Alex C <alex.christianson.05@gmail.com>
2021-10-29 14:40:05 -0500

    add Engineer constructor and tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,23 @@
+const { test, expect } = require('@jest/globals');
+const Engineer = require('../lib/Engineer');
+
+test('can set github username via constructor', () => {
+    const testUsername = "GenericUser";
+    const engineer = new Engineer("Bob", 111, "bob@email.com", testUsername);
+
+    expect(engineer.github).toEqual(testUsername);
+});
+
+test('can get github username from getGithub()', () => {
+    const testUsername = "GenericUser";
+    const engineer = new Engineer("Bob", 111, "bob@email.com", testUsername);
+    
+    expect(engineer.getGithub()).toEqual(testUsername);
+});
+
+test('getRole() should return "Engineer"', () => {
+    const testRole = "Engineer";
+    const engineer = new Engineer("Bob", 111, "bob@email.com", "GenericUsername");
+
+    expect(engineer.getRole()).toEqual(testRole);
+});
